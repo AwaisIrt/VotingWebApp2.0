f957cabc6f47a80dac3263732f3131010cb80db4
Awais Irteza <awais.irteza@vigence.co.uk>
2022-03-27 23:14:30 +0100

    User Authentication

---
+++ Services/Data/DataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VotingWebApp2._0.Models;
+
+namespace VotingWebApp2._0.Services.Data
+{
+    public class DataAccess
+    {
+        internal bool FindByUser(UserLogin user)
+        {
+            //throw new NotImplementedException();
+            //if(user.email == "Admin" && user.password == "Password")
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return (user.email == "Admin" && user.password == "Password");
+        }
+    }
+}
