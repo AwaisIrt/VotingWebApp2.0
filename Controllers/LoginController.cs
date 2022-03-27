361af023b4e1b03bd44c175c96f56bf298366eac
Awais Irteza <awais.irteza@vigence.co.uk>
2022-03-27 22:16:49 +0100

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VotingWebApp2._0.Models;
+
+namespace VotingWebApp2._0.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public string Login(UserLogin userLogin)
+        {
+            return "Result: Username = " + userLogin.email + " Password = "+ userLogin.password;
+        }
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VotingWebApp2._0.Models
+{
+    public class UserLogin
+    {
+        public string email { get; set; }
+        public string password { get; set; }    
+    }
+}
