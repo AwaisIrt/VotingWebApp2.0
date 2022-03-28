0608c4b51f3bc228eaaad3302e1003e25624a352
Awais Irteza <awais.irteza@vigence.co.uk>
2022-03-28 09:14:02 +0100

    Models

---
+++ Models/VoteModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VotingWebApp2._0.Models
+{
+    public class VoteModel
+    {
+        public int voteId { get; set; }
+        public int userId { get; set; }
+        public string campaign { get; set; }
+        public string Candidate { get; set; }
+
+        public VoteModel()
+        {
+            voteId = 0;
+            userId = 0;
+            campaign = string.Empty;
+            Candidate = string.Empty;
+        }
+
+        public VoteModel(int voteId, int userId, string campaign, string candidate)
+        {
+            this.voteId = voteId;
+            this.userId = userId;
+            this.campaign = campaign;
+            Candidate = candidate;
+        }
+    }
+}
