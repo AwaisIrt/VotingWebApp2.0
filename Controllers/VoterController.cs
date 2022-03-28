053728f21217f3af266cef5f06c1eead27f038fc
Awais Irteza <awais.irteza@vigence.co.uk>
2022-03-28 09:15:19 +0100

    Controllers

---
+++ Controllers/VoterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VotingWebApp2._0.Controllers
+{
+    public class VoterController : Controller
+    {
+        // GET: Voter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
